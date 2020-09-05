e1161005c41cab8c3e2258a142e00bb9c3ceed20
colefortner <colefortner@gmail.com>
2020-09-05 19:00:42 -0400

    4. Displayed Table of Satellites

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent {
       new Satellite('SiriusXM', 'Comminication', '2009-03-21', 'LOW', true),
       new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
       new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
-      new Satellite('SiriusXM', 'Comminication', '2001-11-01', 'HIGH', true),
-      new Satellite('SiriusXM', 'Comminication', '1998-11-20', 'LOW', true)
-    ]
+      new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
+      new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true),
+    ];
   }
 }
