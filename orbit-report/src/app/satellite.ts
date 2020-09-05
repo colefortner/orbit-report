938f91d93a3d6bd7e5264e51509ba7b6707a2ee3
colefortner <colefortner@gmail.com>
2020-09-05 17:03:47 -0400

    1. Defined and created satellites

---
+++ orbit-report/src/app/satellite.ts
@@ -0,0 +1,10 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean ){
+        
+    }
+}
