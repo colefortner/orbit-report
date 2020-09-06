bd5bd2c528007ba66caf3b07a5b73620cdea44f8
colefortner <colefortner@gmail.com>
2020-09-05 23:37:15 -0400

    6. Highlighted Space Debris

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -1,15 +1,25 @@
+// import { truncate } from 'fs';
+
 export class Satellite {
     name: string;
     orbitType: string;
     type: string;
     operational: boolean;
     launchDate: string;
+    warning: boolean;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean ){
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
         this.orbitType = orbitType;
         this.type = type;
         this.operational = operational;
         this.launchDate = launchDate;
+    }
+
+    shouldShowWarning(): boolean {
+        if(this.type === 'Space Debris'){
+            return true;
+        }
+        return false;
     }
 }
